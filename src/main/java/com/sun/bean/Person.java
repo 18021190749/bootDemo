afa9072c1c0504f53bccf3c2197fbe335a218d8d
jie <18021190749@189.cn>
2019-05-25 23:17:24 +0800

    1、yaml语法 2、junit测试

---
+++ src/main/java/com/sun/bean/Person.java
@@ -0,0 +1,78 @@
+package com.sun.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+    private String name;
+    private boolean hasHair;
+    private int age;
+    private List<String> lists;
+    private Map<String,Object> maps;
+    private Dog dog;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", hasHair=" + hasHair +
+                ", age=" + age +
+                ", lists=" + lists +
+                ", maps=" + maps +
+                ", dog=" + dog +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isHasHair() {
+        return hasHair;
+    }
+
+    public void setHasHair(boolean hasHair) {
+        this.hasHair = hasHair;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<String> lists) {
+        this.lists = lists;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+}
